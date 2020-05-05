81ea402195c422f0936bd23bf752be6f2d1d2717
Abhijit Gadgil <gabhijit@iitbombay.org>
2020-05-05 22:53:06 +0530

    Fixes examples/tcpdump.py to display packet data

--- examples/tcpdump.py
+++ examples/tcpdump.py
@@ -1,7 +1,9 @@
 import sys
-import time
+from datetime import datetime as dt
+import codecs
+import binascii
 
-MAX_COUNT = 100
+MAX_COUNT = 25
 
 from wishpy.libpcap.lib.libpcap_ext import lib as libpcap_lib
 from wishpy.libpcap.lib.libpcap_ext import ffi as libpcap_ffi
@@ -12,9 +14,12 @@ print(libpcap_ffi.string(pcap_version))
 
 @libpcap_ffi.callback('void (*)(u_char *, const struct pcap_pkthdr *, const u_char *)')
 def wishpy_libpcap_handler(user, hdr, data):
-    print(hdr[0].ts.tv_sec, hdr[0].ts.tv_usec, hdr[0].len, hdr[0].caplen)
-    print(data)
-    #print(user)
+    caplen = hdr[0].caplen
+    #print(hdr[0].ts.tv_sec, hdr[0].ts.tv_usec, hdr[0].len, hdr[0].caplen)
+    total_sec = hdr[0].ts.tv_sec + hdr[0].ts.tv_usec/1000000
+
+    print(dt.strftime(dt.fromtimestamp(total_sec), '%H:%M:%S.%f'), caplen, binascii.hexlify(bytes(data[0:caplen])))
+
 
 err_buffer = libpcap_ffi.new('char [256]')
 print(err_buffer)
@@ -35,12 +40,12 @@ if error != 0 :
     print(libpcap_ffi.string(errstr))
     sys.exit(1)
 
-then = time.time()
+then = dt.now()
 result = libpcap_lib.pcap_loop( pcap_handle, MAX_COUNT,
         wishpy_libpcap_handler, libpcap_ffi.NULL)
 
 print(result)
-now = time.time()
+now = dt.now()
 
 print(now - then)
 
