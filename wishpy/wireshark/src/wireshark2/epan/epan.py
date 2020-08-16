a2a6974835b579853f6fe108fa3daa7a123fee26
Abhijit Gadgil <gabhijit@hyphenos.io>
2020-08-16 16:02:55 +0530

    Filtering support - Wireshark 2.6
    - Added `dfilter_h.py` for 'dfilter' related APIs
    - Fixes in `_wrapper.py` for single packet and multiple packet
      dissection.

--- wishpy/wireshark/src/wireshark2/epan/epan.py
+++ wishpy/wireshark/src/wireshark2/epan/epan.py
@@ -48,6 +48,8 @@ from .epan_dissect_h import epan_epan_dissect_h_types_cdef
 from .proto_h import epan_proto_h_types_cdef
 from .ftypes_h import epan_ftypes_h_types_cdef
 from .ftypes_h import epan_ftypes_h_funcs_cdef
+from .dfilter_h import epan_dfilter_h_types_cdef
+from .dfilter_h import epan_dfilter_h_funcs_cdef
 
 
 epan_ffi = FFI()
@@ -88,6 +90,8 @@ epan_ffi.cdef(epan_address_h_types_cdef)
 epan_ffi.cdef(epan_packet_info_h_cdef)
 epan_ffi.cdef(epan_ftypes_h_types_cdef)
 epan_ffi.cdef(epan_ftypes_h_funcs_cdef)
+epan_ffi.cdef(epan_dfilter_h_types_cdef)
+epan_ffi.cdef(epan_dfilter_h_funcs_cdef)
 epan_ffi.cdef(epan_proto_h_types_cdef)
 epan_ffi.cdef(epan_epan_dissect_h_types_cdef)
 epan_ffi.cdef(epan_h_cdef)
@@ -108,6 +112,7 @@ _sources = '''
             #include <epan/epan.h>
             #include <epan/packet.h>
             #include <epan/ftypes/ftypes.h>
+            #include <epan/dfilter/dfilter.h>
             #include <wsutil/privileges.h>
 
             struct packet_provider_data {
